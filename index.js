c92eef92a1efe026ec92f6871c65f375c1e20dc0
Clemente Estrada <mirdrack@gmail.com>
2016-04-06 15:19:55 -0500

    Added route handlers

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 var express = require('express');
 var app = express();
 
+app.get('/', function (request, response) {
+
+	response.send('<h1>Home Page</h1>');
+});
+
+app.get('/home', function (request, response) {
+
+	response.send('<h1>Home Page</h1>');
+});
+
+app.get('/contact', function (request, response) {
+
+	response.send('Contact page');
+});
+
 app.listen(8000, function () {
 
 	console.log('Server running on port 8000...');
