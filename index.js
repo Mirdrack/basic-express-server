f726c0110547959e9835d586858404ae97b82aae
Clemente Estrada <mirdrack@gmail.com>
2020-11-19 19:11:24 -0600

    Updated tabs by spaces

--- index.js
+++ index.js
@@ -3,20 +3,20 @@ var app = express();
 
 app.get('/', function (request, response) {
 
-	response.send('<h1>Home Page</h1>');
+    response.send('<h1>Home Page</h1>');
 });
 
 app.get('/home', function (request, response) {
 
-	response.send('<h1>Home Page</h1>');
+    response.send('<h1>Home Page</h1>');
 });
 
 app.get('/contact', function (request, response) {
 
-	response.send('Contact page');
+    response.send('Contact page');
 });
 
 app.listen(8000, function () {
 
-	console.log('Server running on port 8000...');
+    console.log('Server running on port 8000...');
 });
